de02dea9fa8f383aaf54315fb442941d7707e04d
seunggeun <bsg1017@naver.com>
2022-10-31 10:16:15 +0900

    프로그래머스 모의고사 문제 -1st commit

---
+++ src/MockExamSolution.java
@@ -0,0 +1,25 @@
+public class MockExamSolution {
+
+    public int[] solution(int[] answers) {
+
+        int[] answer = {};
+
+        int[] studenta = {1, 2, 3, 4, 5};
+        //a학생
+        int[] studentb = {2, 1, 2, 3, 2, 4, 2, 5};
+        //b학생
+        int[] studentc = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        //c학생
+
+        int[] score = {0, 0, 0};
+
+        for (int i = 0; i < 3 ; i++) {
+            for (int j = 0; j < answers.length ; j++) {
+
+            }
+        }
+
+        return answer;
+    }
+
+}
