006e4d28d3573fe21e6be4a6cd96fa12b1ffc768
seunggeun <bsg1017@naver.com>
2022-11-09 09:37:12 +0900

    알고리즘 선택정렬 -2nd commit -i가 0,1,2일 때 arr[0,1,2]에 들어갈  최소값이 들어있는 index 찾기

--- src/SelectionSort/Solution.java
+++ src/SelectionSort/Solution.java
@@ -1,5 +1,7 @@
 package SelectionSort;
 
+import java.util.Arrays;
+
 public class Solution {
     public static void main(String[] args) {
 
@@ -9,10 +11,40 @@ public class Solution {
         // arr[0]번쨰에 들어갈 값 찾기
         // 그 값이 들어있는 index 찾기 --0
         int minIdx =0;
+        for (int j =0; j < arr.length; j++) {
+            if(arr[minIdx] > arr[j]) minIdx =j;
+
+        }
+        int temp = arr[0];
+        arr[0] = arr[minIdx];
+        arr[minIdx] = temp;
+        System.out.println(Arrays.toString(arr));
 
         i=1;
         // arr[1]번쨰에 들어갈 값 찾기
-        // 그 값이 들어있는 index 찾기 --1
+        // 그 값이 들어있는 index 찾기 -- 8
+        // 0번은 정렬이 끝났기 때문에 7, 4, 9, 10, 223, 111, 23, 3, 39 만 생각
+        minIdx =1;
+        for (int j =1; j < arr.length; j++) {
+            if(arr[minIdx] > arr[j]) minIdx =j;
+
+        }
+        temp = arr[1];
+        arr[1] = arr[minIdx];
+        arr[minIdx] = temp;
+        System.out.println(Arrays.toString(arr));
+
+        i=2;
+        //arr[2]번째에 들어갈 값 찾기
+        minIdx =2;
+        for (int j =2; j < arr.length; j++) {
+            if(arr[minIdx] > arr[j]) minIdx =j;
+
+        }
+        temp = arr[2];
+        arr[2] = arr[minIdx];
+        arr[minIdx] = temp;
+        System.out.println(Arrays.toString(arr));
 
     }
 }
