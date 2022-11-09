70fd5e9363a4cc4b694354ebb1d0b7ac442c68da
seunggeun <bsg1017@naver.com>
2022-11-09 09:16:52 +0900

    알고리즘 선택정렬 -초기셋팅 -i가 0일 때 arr[0]에 들어갈 최소값이 들어있는 index 찾기

---
+++ src/SelectionSort/Solution.java
@@ -0,0 +1,18 @@
+package SelectionSort;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        int arr[] = {2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
+
+        int i=0;
+        // arr[0]번쨰에 들어갈 값 찾기
+        // 그 값이 들어있는 index 찾기 --0
+        int minIdx =0;
+
+        i=1;
+        // arr[1]번쨰에 들어갈 값 찾기
+        // 그 값이 들어있는 index 찾기 --1
+
+    }
+}
