5e290131f112214fbd501f91086713311dcd0e2a
geunnseung <bsg1017@naver.com>
2022-11-15 09:17:51 +0900

    java algorithm - quick sort 초기세팅

---
+++ src/QuickSort/Solution.java
@@ -0,0 +1,11 @@
+package QuickSort;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        var arr = new int[]{20, 18, 5, 19, 5, 25, 40, 50};
+
+        // 1. 기준값 뽑는 로직 구현
+        // 2. 기준값 기준으로 왼쪽 오른쪽으로 나누어 담는 로직 구현
+    }
+}
